9606c1902c13eb097f591e1bf6dfa13f92bc41bb
wendyleung <wendysarahleung@gmail.com>
2015-02-13 15:44:19 -0800

    complete enemies move randomly every second deliverable

--- watchout.js
+++ watchout.js
@@ -1,11 +1,24 @@
 // start slingin' some d3 here.
+var width = 800;
+var height = 600;
 
 var container = d3.selectAll('body').append('svg')
-  .attr('width', 800)
-  .attr('height', 600)
+  .attr('width', width)
+  .attr('height', height)
   .style("border", "1px solid black")
 
-var imgs = container.selectAll("image").data([0,1,2,3,4,5,6,7,8,9]);
+
+// function to fill asteroid data array
+var generateArray = function (n) {
+  var generatedArray = [];
+  for (var i = 0; i < n; i++) {
+    generatedArray.push(i);
+  }
+  return generatedArray
+}
+
+var imgs = container.selectAll("image")
+  .data(generateArray(15));
   imgs.enter()
   .append("svg:image")
   .attr("class", "enemy")
@@ -14,3 +27,19 @@ var imgs = container.selectAll("image").data([0,1,2,3,4,5,6,7,8,9]);
   .attr("y", "0") // put in circle - sin and cos
   .attr("width", "20")
   .attr("height", "20");
+
+function update(data) {
+/*  var enemies = container.selectAll('imgs')
+    .data(data, function(d) { return d; })
+*/
+  //UPDATE ENEMIES POSITION
+  data.transition().duration(1000)
+    .attr("x", function(d) { return Math.floor(Math.random() * width);})
+    .attr("y", function(d) { return Math.floor(Math.random() * height);})
+
+}
+update(imgs);
+
+setInterval(function() {
+  update(imgs);
+}, 1000)
