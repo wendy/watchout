5be03fe2308a918c30f4c8b3ec95220af374b5b7
wendyleung <wendysarahleung@gmail.com>
2015-02-13 16:05:28 -0800

    complete user dot piece

--- watchout.js
+++ watchout.js
@@ -28,6 +28,26 @@ var imgs = container.selectAll("image")
   .attr("width", "20")
   .attr("height", "20");
 
+// drag effect for user's circle piece
+var drag = d3.behavior.drag()
+  .on('dragstart', function () { circle.style('fill', 'pink');})
+  .on('drag', function() { circle.attr('cx', d3.event.x)
+      .attr('cy', d3.event.y);})
+  .on('dragend', function() {circle.style('fill', 'black');});
+
+// declare user's circle piece
+var circle = container.selectAll('.draggableCircle')
+  .data([{ x: (width/2), y: (height/2), r: 10}])
+  .enter()
+  .append('svg:circle')
+  .attr('class', 'draggableCircle')
+  .attr('cx', function(d) {return d.x;})
+  .attr('cy', function(d) {return d.y;})
+  .attr('r', function(d) {return d.r;})
+  .call(drag)
+  .style('fill', 'black');
+
+
 function update(data) {
 /*  var enemies = container.selectAll('imgs')
     .data(data, function(d) { return d; })
