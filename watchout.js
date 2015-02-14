2c142695165fd016033ff2f122b1c7f0c3f726bd
wendyleung <wendysarahleung@gmail.com>
2015-02-13 18:55:19 -0800

    complete collision detection deliverable

--- watchout.js
+++ watchout.js
@@ -23,7 +23,8 @@ var imgs = container.selectAll("image")
   .append("svg:image")
   .attr("class", "enemy")
   .attr("xlink:href", "asteroid.png")
-  .attr("x", function(d){ return d * 30})
+  // .attr("x", function(d){ return d * 30})
+  .attr("x", "0")
   .attr("y", "0") // put in circle - sin and cos
   .attr("width", "20")
   .attr("height", "20");
@@ -62,4 +63,33 @@ update(imgs);
 
 setInterval(function() {
   update(imgs);
-}, 1000)
+}, 1000);
+
+setInterval(function() {
+  checkCollisions();
+}, 100)
+
+var collided = 0;
+var checkCollisions = function(){
+  var Cx = circle.attr("cx");
+  var Cy = circle.attr("cy");
+
+  for( var i = 0; i < imgs[0].length; i++ ){
+    var Ex = imgs[0][i].x.animVal.value + 10;
+    var Ey = imgs[0][i].y.animVal.value + 10;
+    //console.log(Cx, Cy);
+    //console.log(Ex, Ey);
+
+    //calculate distance between enemy and circle
+    var distance = Math.sqrt(Math.pow(Cx - Ex, 2) + Math.pow(Cy - Ey, 2));
+    // if the distance between circle and enemy is < 20 px
+    if (distance < 20) {
+    //   they have collided. terrible
+    //   if score > highscore
+    //     set highscore to score
+    //   set score back to zero
+    //   incriment collisions
+    collided += 1;
+    }
+  }
+};
